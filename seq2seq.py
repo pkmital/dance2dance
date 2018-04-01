0547c03cc011c65db4564468917b7c27e05c844c
pkmital <parag@pkmital.com>
2018-04-01 03:36:08 +0000

    removing MDN for now; simpler regression network; added inference

--- seq2seq.py
+++ seq2seq.py
@@ -1,7 +1,55 @@
-import numpy as np
+from functools import partial
 import tensorflow as tf
 import tensorflow.contrib.rnn as rnn
 import tensorflow.contrib.distributions as tfd
+
+
+class RegressionHelper(tf.contrib.seq2seq.Helper):
+    """Helper interface.    Helper instances are used by SamplingDecoder."""
+    def __init__(self, batch_size, max_sequence_size, n_features):
+        self._batch_size = batch_size
+        self._max_sequence_size = max_sequence_size
+        self._n_features = n_features
+        self._batch_size_tensor = tf.convert_to_tensor(
+                batch_size, dtype=tf.int32, name="batch_size")
+
+    @property
+    def batch_size(self):
+        """Returns a scalar int32 tensor."""
+        return self._batch_size_tensor
+
+    @property
+    def sample_ids_dtype(self):
+        return tf.float32
+
+    @property
+    def sample_ids_shape(self):
+        return self._n_features
+
+    def initialize(self, name=None):
+        finished = tf.tile([False], [self._batch_size])
+        start_inputs = tf.fill([self._batch_size, self._n_features], 0.0)
+        return (finished, start_inputs)
+
+    def sample(self, time, outputs, state, name=None):
+        """Returns `sample_ids`."""
+        del time, state
+        return outputs
+
+    def next_inputs(self, time, outputs, state, sample_ids, name=None):
+        """Returns `(finished, next_inputs, next_state)`."""
+        del sample_ids
+        finished = tf.cond(
+            tf.less(time, self._max_sequence_size),
+            lambda: False, lambda: True)
+        del time
+        all_finished = tf.reduce_all(finished)
+        next_inputs = tf.cond(
+            all_finished,
+            # If we're finished, the next_inputs value doesn't matter
+            lambda: tf.zeros_like(outputs),
+            lambda: outputs)
+        return (finished, next_inputs, state)
 
 
 def gausspdf(x, mean, sigma):
@@ -51,13 +99,14 @@ def _create_encoder(source, lengths, batch_size, n_enc_neurons, n_layers,
 
     # Now hookup the cells to the input
     # [batch_size, max_time, embed_size]
-    (outputs_fw, output_bw), (final_state_fw, final_state_bw) = tf.nn.bidirectional_dynamic_rnn(
-        cell_fw=cell_fw,
-        cell_bw=cell_bw,
-        inputs=source,
-        sequence_length=lengths,
-        time_major=False,
-        dtype=tf.float32)
+    (outputs_fw, output_bw), (final_state_fw, final_state_bw) = \
+        tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=cell_fw,
+            cell_bw=cell_bw,
+            inputs=source,
+            sequence_length=lengths,
+            time_major=False,
+            dtype=tf.float32)
 
     return outputs_fw, final_state_fw
 
@@ -74,6 +123,7 @@ def _create_decoder(n_dec_neurons,
                     n_features,
                     scope,
                     max_sequence_size,
+                    n_gaussians,
                     use_attention=False):
     from tensorflow.python.layers.core import Dense
     output_layer = Dense(n_features, name='output_projection')
@@ -110,7 +160,21 @@ def _create_decoder(n_dec_neurons,
         impute_finished=True,
         maximum_iterations=max_sequence_size)
 
-    return outputs
+    helper = RegressionHelper(
+        batch_size=batch_size, max_sequence_size=max_sequence_size, n_features=n_features)
+    scope.reuse_variables()
+    infer_decoder = tf.contrib.seq2seq.BasicDecoder(
+        cell=cells,
+        helper=helper,
+        initial_state=initial_state,
+        output_layer=output_layer)
+    infer_outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+        infer_decoder,
+        output_time_major=False,
+        impute_finished=True,
+        maximum_iterations=max_sequence_size)
+    # infer_logits = tf.identity(infer_outputs.sample_id, name='infer_logits')
+    return outputs, infer_outputs
 
 
 def create_model(batch_size=50,
@@ -131,7 +195,7 @@ def create_model(batch_size=50,
     target = tf.placeholder(
         tf.float32,
         shape=(batch_size, sequence_length, n_features),
-        name='source')
+        name='target')
     lengths = tf.multiply(
         tf.ones((batch_size,), tf.int32),
         sequence_length - 1,
@@ -153,23 +217,6 @@ def create_model(batch_size=50,
         source_embed, source_embed_matrix = _create_embedding(
             x=source, embed_size=input_embed_size)
 
-    # Embed word ids for target embedding
-    with tf.variable_scope('target/embedding'):
-        # Check if we need a new embedding matrix or not.  If we are for
-        # instance translating to another language, then we'd need different
-        # vocabularies for the input and outputs, and so new embeddings.
-        # However if we are for instance building a chatbot with the same
-        # language, then it doesn't make sense to have different embeddings and
-        # we should share them.
-        if share_input_and_target_embedding:
-            target_input_embed, target_embed_matrix = _create_embedding(
-                x=decoder_input,
-                embed_size=target_embed_size,
-                embed_matrix=source_embed_matrix)
-        else:
-            target_input_embed, target_embed_matrix = _create_embedding(
-                x=decoder_input, embed_size=target_embed_size)
-
     # Build the encoder
     with tf.variable_scope('encoder'):
         encoder_outputs, encoder_state = _create_encoder(
@@ -184,9 +231,7 @@ def create_model(batch_size=50,
 
     # Build the decoder
     with tf.variable_scope('decoder') as scope:
-        n_outputs = n_features * n_gaussians + (
-            n_features * n_features) * n_gaussians + n_gaussians
-        decoding = _create_decoder(
+        outputs, infer_outputs = _create_decoder(
             n_dec_neurons=n_neurons,
             n_layers=n_layers,
             keep_prob=keep_prob,
@@ -196,47 +241,15 @@ def create_model(batch_size=50,
             encoder_lengths=lengths,
             decoding_inputs=decoder_input,
             decoding_lengths=lengths,
-            n_features=n_outputs,
+            n_features=n_features,
             scope=scope,
-            max_sequence_size=sequence_length - 1)
-
-    with tf.variable_scope('mdn'):
-        means = tf.reshape(
-            tf.slice(
-                decoding[0], [0, 0, 0],
-                [batch_size, sequence_length - 1, n_features * n_gaussians]),
-            [batch_size, sequence_length - 1, n_features, n_gaussians])
-        sigmas = tf.nn.softplus(tf.reshape(
-            tf.slice(decoding[0], [0, 0, n_features * n_gaussians], [
-                batch_size, sequence_length - 1,
-                n_features * n_gaussians
-            ]), [
-                batch_size, sequence_length - 1, n_features,
-                n_gaussians
-            ]))
-        weights = tf.nn.softmax(tf.reshape(
-            tf.slice(
-                decoding[0],
-                [0, 0, n_features * n_gaussians + n_features * n_features * n_gaussians],
-                [batch_size, sequence_length - 1, n_gaussians]),
-            [batch_size, sequence_length - 1, n_gaussians]))
-        components = []
-        for gauss_i in range(n_gaussians):
-            mean_i = means[:, :, :, gauss_i]
-            sigma_i = sigmas[:, :, :, gauss_i]
-            components.append(
-                tfd.MultivariateNormalDiag(
-                    loc=mean_i, scale_diag=sigma_i))
-        gauss = tfd.Mixture(
-            cat=tfd.Categorical(probs=weights), components=components)
+            max_sequence_size=sequence_length - 1,
+            n_gaussians=n_gaussians)
 
     with tf.variable_scope('loss'):
-        p = gauss.prob(decoder_output)
-        negloglike = -tf.log(tf.maximum(p, 1e-10))
-        weighted_reconstruction = tf.reduce_mean(tf.expand_dims(weights, 2) * means, 3)
-        mdn_loss = tf.reduce_mean(tf.reduce_sum(negloglike, 1))
+        mdn_loss = tf.reduce_mean(tf.reduce_sum([[0.0]], 1))
         mse_loss = tf.losses.mean_squared_error(
-            weighted_reconstruction, decoder_output)
+            outputs[0], decoder_output)
         loss = mdn_loss + mse_loss
 
     return {
@@ -244,8 +257,7 @@ def create_model(batch_size=50,
         'target': target,
         'keep_prob': keep_prob,
         'encoding': encoder_state,
-        'decoding': decoding,
-        'p': p,
+        'decoding': infer_outputs,
         'loss': loss,
         'mdn_loss': mdn_loss,
         'mse_loss': mse_loss
--- train.py
+++ train.py
@@ -9,8 +9,13 @@ def batch_generator(data, sequence_length, batch_size=50):
     for batch_i in range(n_batches):
         this_idxs = idxs[batch_i * batch_size:(batch_i + 1) * batch_size]
         source = [data[i:i + sequence_length, :] for i in this_idxs]
-        target = [data[i + sequence_length:i + sequence_length * 2, :] for i in this_idxs]
-        yield np.array(source, dtype=np.float32), np.array(target, dtype=np.float32)
+        target = [
+            data[i + sequence_length:i + sequence_length * 2, :]
+            for i in this_idxs
+        ]
+        yield np.array(
+            source, dtype=np.float32), np.array(
+                target, dtype=np.float32)
 
 
 def train(data, n_epochs=1000, batch_size=100, sequence_length=240, **kwargs):
@@ -43,7 +48,8 @@ def train(data, n_epochs=1000, batch_size=100, sequence_length=240, **kwargs):
             if it_i % 1000 == 0:
                 current_learning_rate = max(0.0001,
                                             current_learning_rate * 0.99)
-                print('iteration: {}, learning rate: {}'.format(it_i, current_learning_rate))
+                print('iteration: {}, learning rate: {}'.format(
+                    it_i, current_learning_rate))
             mse_loss, mdn_loss, _ = sess.run(
                 [net['mse_loss'], net['mdn_loss'], opt],
                 feed_dict={
@@ -56,21 +62,16 @@ def train(data, n_epochs=1000, batch_size=100, sequence_length=240, **kwargs):
             total_mdn += mdn_loss
             total_mse += mse_loss
             print('{}: mdn: {}, mse: {}, total: {}'.format(
-                it_i,
-                mdn_loss,
-                mse_loss,
-                mdn_loss + mse_loss, end='\r'))
-        print('-- epoch {}: mdn: {}, mse: {}, total: {} --'.format(
-            epoch_i,
-            total_mdn / (it_i + 1),
-            total_mse / (it_i + 1),
+                it_i, mdn_loss, mse_loss, mdn_loss + mse_loss, end='\r'))
+        print('\n-- epoch {}: mdn: {}, mse: {}, total: {} --\n'.format(
+            epoch_i, total_mdn / (it_i + 1), total_mse / (it_i + 1),
             total / (it_i + 1)))
-        saver.save(sess, './seq2seq.ckpt', global_step=it_i)
+        saver.save(sess, './seq2seq.ckpt', global_step=epoch_i)
 
     sess.close()
 
 
-def run_euler():
+def quat():
     data = np.load('quats.npy')
     data = (data.reshape([data.shape[0], -1]) - np.mean(data)) / np.std(data)
     batch_size = 32
@@ -83,8 +84,53 @@ def run_euler():
     n_layers = 2
     n_gaussians = 10
     use_attention = True
-    train(**locals())
+    return locals()
+
+
+def run_inference():
+    data = np.load('quats.npy')
+    mean_data = np.mean(data)
+    std_data = np.std(data)
+    data = (data.reshape([data.shape[0], -1]) - mean_data) / std_data
+    batch_size = 32
+    sequence_length = 120
+    n_features = data.shape[-1]
+    input_embed_size = 512
+    target_embed_size = 512
+    share_input_and_target_embedding = True
+    n_neurons = 512
+    n_layers = 2
+    n_gaussians = 10
+    use_attention = True
+    n_features = data.shape[1]
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+
+    net = seq2seq.create_model(
+        batch_size=batch_size,
+        n_features=n_features,
+        sequence_length=sequence_length)
+
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(
+        net['loss'])
+    init_op = tf.group(tf.global_variables_initializer(),
+                       tf.local_variables_initializer())
+    sess.run(init_op)
+    saver = tf.train.Saver()
+    saver.restore(sess, 'seq2seq.ckpt-40')
+    source, target = next(batch_generator(data, sequence_length=sequence_length, batch_size=batch_size))
+    recon = sess.run(net['decoding'], feed_dict={
+        net['source']: source,
+        net['keep_prob']: 1.0})
+    res = (recon[0] * std_data) + mean_data
+    tgt = (target[0] * std_data) + mean_data
+    fig, axs = plt.subplots(1, 2)
+    axs[0].plot(tgt)
+    axs[1].plot(res[0])
 
 
 if __name__ == '__main__':
-    run_euler()
+    params = quat()
+    train(**params)
